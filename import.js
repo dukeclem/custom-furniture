ddb182d274228cd34ca083e378a4d2989068c00c
ronperris <ronperris@gmail.com>
2015-05-27 14:27:22 -0700

    Added database import script.

---
+++ import.js
@@ -0,0 +1,22 @@
+/**
+ * Created by ronperris on 5/27/15.
+ */
+var furniture = {
+    "furnitures": [
+        {
+            "name": "Danish Table",
+            "style": "Danish",
+            "room": "Hallway",
+            "type": "credenza",
+            "price": "1000",
+            "material": "Teak Wood",
+            "height": "32",
+            "width": "56",
+            "depth": "24"
+            "care": "Wipe up spills immediately. Dust often using a soft, dry, lint-free cloth. Wipe occasionally with a soft cloth dampened with a solution of warm water and mild detergent, then dry immediately with a lint-free cloth. TOUCH-UP CRAYON A special crayon is included with ebonized oak versions to touch up nicks and scratches. Run the crayon along the affected area, then buff with a soft, dry, lint-free coth to remove excess material."
+        },
+        {
+
+        }
+    ]
+};
